2aeb89b020ef8bb4fe528636efcbcceed2326d16
Deepak Azad <dazad>
2012-11-09 06:36:45 -0800

    Seaglass Look and feel

--- MeFi/src/ubc/cpsc544/PublicDashboard.java
+++ MeFi/src/ubc/cpsc544/PublicDashboard.java
@@ -14,6 +14,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
+import javax.swing.UIManager;
 import javax.swing.border.LineBorder;
 
 public class PublicDashboard {
@@ -26,6 +27,12 @@ public class PublicDashboard {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		try {
+		    UIManager.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
